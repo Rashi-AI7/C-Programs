7bb8fb4338ed52bb47a48554fc93364fc7353113
RASHI-AI7 <rashisingh.bt@gmail.com>
2025-05-11 16:04:05 +0530

    Added bubble sort code

--- Strings/strg1.c
+++ Strings/strg1.c
@@ -4,16 +4,18 @@
 int countLen(char arr[]);
 
 int main(){
-    char name[100];
-    fgets(name, 100, stdin);
+    char name[100];  // Declaring a character array (string) with a maximum size of 100
+    fgets(name, 100, stdin);  // Reading a string input from the user, including spaces (fgets prevents buffer overflow)
     printf("Length is %d", countLen(name));
     return 0;
     }
 
+
+// Function to count the length of the string manually
 int countLen(char arr[]){
     int count = 0;
-    for(int i=0; arr[i] != '\0'; i++){
+    for(int i=0; arr[i] != '\0'; i++){  // Loop through the array until the null character ('\0') is found
         count++;
     }
-    return count-1;
+    return count-1;  // Return count - 1 to exclude the newline character (`\n`) added by fgets
 }
